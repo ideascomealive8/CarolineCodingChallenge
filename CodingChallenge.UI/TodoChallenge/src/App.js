298e19a61acb6f5af174a5c832e871e628bbf609
ideascomealive8 <145354922+ideascomealive8@users.noreply.github.com>
2023-09-29 02:12:21 +0500

    Update App.js

--- CodingChallenge.UI/TodoChallenge/src/App.js
+++ CodingChallenge.UI/TodoChallenge/src/App.js
@@ -1,6 +1,9 @@
-import React, {Component} from 'react';
-import TodoList from "./components/todo/TodoList";
-import "./App.scss";
+import React, { Component } from 'react';
+import Chart from 'chart.js/auto';
+import TodoList from './components/todo/TodoList';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSave } from '@fortawesome/free-solid-svg-icons';
+import './App.scss';
 import './button.scss';
 
 class App extends Component {
@@ -8,26 +11,91 @@ class App extends Component {
     super(props);
 
     this.state = {
-      newTodo: ''
+      newTodo: '',
+      dueDate: '', // Add dueDate to state
+      todos: [],
     };
   }
 
-  textInputChange = (e) =>  {
-    this.setState({...this.state, newTodo: e.target.value});
+  componentDidMount() {
+    const storedTodos = localStorage.getItem('todos');
+    if (storedTodos) {
+      this.setState({ todos: JSON.parse(storedTodos) });
+    }
+
+    const chartCanvas = document.getElementById('todoChart').getContext('2d');
+
+    const numComplete = this.state.todos.filter((todo) => todo.completed).length;
+    const numIncomplete = this.state.todos.length - numComplete;
+
+    new Chart(chartCanvas, {
+      type: 'doughnut',
+      data: {
+        labels: ['Complete', 'Incomplete'],
+        datasets: [
+          {
+            data: [numComplete, numIncomplete],
+            backgroundColor: ['#28a745', '#dc3545'],
+          },
+        ],
+      },
+    });
+  }
+
+  textInputChange = (e) => {
+    this.setState({ ...this.state, newTodo: e.target.value });
+  }
+
+  dueDateInputChange = (e) => {
+    this.setState({ ...this.state, dueDate: e.target.value }); // Add due date input change handler
   }
 
   addNewTodo = () => {
-    console.warn('not implemented');
+    const { newTodo, dueDate } = this.state;
+
+    if (newTodo.trim() !== '') {
+      const newTodoItem = {
+        id: Date.now(),
+        text: newTodo,
+        completed: false,
+        dueDate, // Include the due date
+      };
+
+      this.setState(
+        (prevState) => ({
+          todos: [...prevState.todos, newTodoItem],
+          newTodo: '',
+          dueDate: '', // Clear the due date input
+        }),
+        () => {
+          localStorage.setItem('todos', JSON.stringify(this.state.todos));
+        }
+      );
+    }
   }
 
   render() {
     return (
       <div className="App">
-        <input type="text" value={this.state.newTodo} onChange={this.textInputChange}></input>
-        <button className={"btn--default"} onClick={this.addNewTodo}>Add</button>
-        <TodoList />
+        <canvas id="todoChart" width="200" height="200"></canvas>
+        <input
+          type="text"
+          value={this.state.newTodo}
+          onChange={this.textInputChange}
+          placeholder="Add a new todo"
+        />
+        <input
+          type="date"
+          value={this.state.dueDate} // Add input for due date
+          onChange={this.dueDateInputChange}
+        />
+        <button className="btn--default" onClick={this.addNewTodo}>
+          <FontAwesomeIcon icon={faSave} /> Add
+        </button>
+        <TodoList todos={this.state.todos} />
       </div>
-  )}
+    );
+  }
 }
 
 export default App;
