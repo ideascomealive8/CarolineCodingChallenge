fd140b9c1f3d62b6c149dbdd0961847c4ce945ba
ideascomealive8 <145354922+ideascomealive8@users.noreply.github.com>
2023-09-29 02:11:13 +0500

    Update TodoList.js

--- CodingChallenge.UI/TodoChallenge/src/components/todo/TodoList.js
+++ CodingChallenge.UI/TodoChallenge/src/components/todo/TodoList.js
@@ -1,10 +1,10 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Todo from "./Todo";
-import {TodoListModel} from "../../TodoModel";
-import {connect} from "react-redux";
-import {completeTodo, getTodos, TODO_TEXT_CHANGE} from "../../todoActions";
+import { TodoListModel } from "../../TodoModel";
+import { connect } from "react-redux";
+import { completeTodo, getTodos, TODO_TEXT_CHANGE, TODO_DUE_DATE_CHANGE } from "../../todoActions"; // Import the new action type
 
-const TodoList = ({todos, getTodos, onTodoTextChange, onTodoCompleteChange}) => {
+const TodoList = ({ todos, getTodos, onTodoTextChange, onTodoCompleteChange, onTodoDueDateChange }) => {
     const [filtered, setFiltered] = useState(true);
 
     const filterByOnChange = () => {
@@ -19,17 +19,21 @@ const TodoList = ({todos, getTodos, onTodoTextChange, onTodoCompleteChange}) => 
         return todos.filter(filterTodos).map(mapTodoObjectToComponent);
     }
     const filterTodos = (todo) => filtered ? !todo.isComplete : true;
-    const mapTodoObjectToComponent = (todo, i) => (<Todo key={i}
-                                                         todo={todo}
-                                                         onTextChange={onTodoTextChange}
-                                                         onCompleteChange={onTodoCompleteChange} />);
+    const mapTodoObjectToComponent = (todo, i) => (
+        <Todo key={i}
+            todo={todo}
+            onTextChange={onTodoTextChange}
+            onCompleteChange={onTodoCompleteChange}
+            onDueDateChange={onTodoDueDateChange}
+        />
+    );
 
     return (
         <div className="todo-list">
             <h2>List of todos</h2>
             <div>
                 <span>Filter by complete</span>
-                <input type="checkbox" defaultChecked={filtered} onChange={filterByOnChange}  />
+                <input type="checkbox" defaultChecked={filtered} onChange={filterByOnChange} />
             </div>
             {renderTodoList(todos)}
         </div>
@@ -42,8 +46,9 @@ const mapStateToProps = (state) => ({
     todos: state.todos ?? []
 });
 const mapDispatchToProps = (dispatch) => ({
-    onTodoTextChange: (text, id) => dispatch({type: TODO_TEXT_CHANGE, text, id}),
+    onTodoTextChange: (text, id) => dispatch({ type: TODO_TEXT_CHANGE, text, id }),
     onTodoCompleteChange: (todo) => dispatch(completeTodo(todo)),
+    onTodoDueDateChange: (dueDate, id) => dispatch({ type: TODO_DUE_DATE_CHANGE, dueDate, id }),
     getTodos: () => dispatch(getTodos())
 });
 
